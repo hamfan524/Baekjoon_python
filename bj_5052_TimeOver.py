4dd9393949f4a597c74fd1cf4d6424fad1de5bf2
dongho <hamfan524@naver.com>
2021-06-11 03:29:06 +0900

    Tree

---
+++ bj_5052_TimeOver.py
@@ -0,0 +1,19 @@
+def solution(numbers):
+  numbers.sort()
+  for i in range(len(numbers)-1):           # 정렬되어 있으므로 i번째는 i+1번째와만 비교해보면 된다.
+    if numbers[i] in numbers[i+1][0:]:
+      print("NO")
+      return False
+    print("Yes")
+    return True
+
+
+numbers = []
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  for _ in range(n):
+    numbers.append(input())
+  solution(numbers)
+  numbers.clear()
