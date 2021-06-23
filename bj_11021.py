af778f47b88f91328184c56f7778ead9438b1958
hamfan524 <hamfan524@naver.com>
2021-06-23 23:51:47 +0900

    study1

--- bj_11021.py
+++ bj_11021.py
@@ -2,8 +2,8 @@ t = int(input())
 sum_case = []
 
 for i in range(t):
-  a ,b = list(map(int, input().split()))
+  a ,b = map(int, input().split())
   sum_case.append(a + b)
 
 for i in range(t):
-  print("Case #{}: {}".format(i + 1,sum_case[i]))
+  print(f"Case #{i+1}: {sum_case[i]}")
--- bj_15552.py
+++ bj_15552.py
@@ -1,11 +1,12 @@
 import sys
-t = sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
+t = int(input().rstrip())
 sum_case = []
 
-for i in range(int(t)):
-  a, b = list(map(int , sys.stdin.readline().split()))
+for i in range(t):
+  a, b = map(int , input().split())
   sum_case.append(a + b)
 
-for i in range(int(t)):
+for i in range(t):
   print(sum_case[i])
