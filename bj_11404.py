92f4e16b3fefbb7df6323e923f94f41d7ee34720
hamfan524 <hamfan524@naver.com>
2021-06-26 03:19:22 +0900

    Floyd-Warshall

--- bj_11404.py
+++ bj_11404.py
@@ -5,20 +5,26 @@ inf = sys.maxsize
 
 n = int(input())
 m = int(input())
-graph = [[inf] * n for i in range(n)]
+
+#최단 경로를 담는 배열
+dist = [[inf] * n for _ in range(n)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    if graph[a-1][b-1] > c:
-        graph[a-1][b-1] = c
+    if dist[a-1][b-1] > c:
+        dist[a-1][b-1] = c
 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                graph[i][j] = 0
-            elif graph[i][j] > graph[i][k] + graph[k][j]:
-                graph[i][j] = graph[i][k] + graph[k][j]
+for k in range(n):          #거치는 점
+    for i in range(n):         #시작 점
+        for j in range(n):        #끝 점
+                # k를 거쳤을 때 더 적은 경로
+            if i != j and dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
 
-for i in range(n):
-    print(*graph[i])
+for i in dist:
+    for j in i:
+        if j == inf:
+            print(0, end=" ")
+        else:
+            print(j, end=" ")
+    print()
