3c97987872b2a983066be6d3139738dcd4a9fc2a
hamfan524 <hamfan524@naver.com>
2021-06-26 02:56:37 +0900

    Floyd-Warshall

---
+++ bj_11404.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+inf = sys.maxsize
+
+n = int(input())
+m = int(input())
+graph = [[inf] * n for i in range(n)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if graph[a-1][b-1] > c:
+        graph[a-1][b-1] = c
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                graph[i][j] = 0
+            elif graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+for i in range(n):
+    print(*graph[i])
