29f3f9c11c6dfa3ab1fafc03a75ce3a3c654ba45
hamfan524 <hamfan524@naver.com>
2021-06-30 02:00:13 +0900

    DP

--- bj_20500_2.py
+++ bj_20500_2.py
@@ -1,9 +1,9 @@
 n = int(input())
-d = [0]*1516
+dp = [0] * (n + 1)
 
-for i in range(2,n+1):
-    if i%2 == 0:
-        d[i] = (d[i-1] * 2 + 1) % 1000000007
+for i in range(2, n + 1):
+    if i % 2 == 0:
+        dp[i] = (dp[i-1] * 2 + 1) % 1000000007
     else:
-        d[i] = (d[i-1] * 2 - 1) % 1000000007
-print(d[n])
+        dp[i] = (dp[i-1] * 2 - 1) % 1000000007
+print(dp[n])
